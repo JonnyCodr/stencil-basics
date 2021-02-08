2c9d1c6c33317a6037a1807c4b2c2b27ad28e428
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2021-02-08 10:03:15 -0700

    removes unnecessary code/ files

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -2,31 +2,10 @@ import { Component, Prop, h } from '@stencil/core';
 import { format } from '../../utils/utils';
 
 @Component({
-  tag: 'my-component',
+  tag: 'side-drawer',
   styleUrl: 'side-drawer.css',
   shadow: true,
 })
 export class SideDrawer {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
 
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
-
-  /**
-   * The last name
-   */
-  @Prop() last: string;
-
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
-  }
-
-  render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
-  }
 }
