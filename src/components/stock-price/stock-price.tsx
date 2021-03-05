64d3d3035f165826490186fb6108a3effe182c83
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2021-03-04 20:20:46 -0700

    adds 2-way-binding and disables button if input is not valid

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component, h, State, Element } from '@stencil/core';
+import { Component, h, State  /*Element*/ } from '@stencil/core';
 import axios from 'axios';
 import { AV_API_KEY } from '../../global';
 
@@ -10,13 +10,24 @@ import { AV_API_KEY } from '../../global';
 })
 export class StockPrice {
   stockInput: HTMLInputElement;
-  // @Element() el: HTMLElement;
+
   @State() fetchedPrice: number;
+  @State() stockUserInput: string;
+  @State() stockInputValid = false;
+
+  onUserInput(event: Event) {
+    this.stockUserInput = (event.target as HTMLInputElement).value;
+    if (this.stockUserInput.trim() !== '') {
+      this.stockInputValid = true;
+    } else {
+      this.stockInputValid = false;
+    }
+  }
 
   async onFetchStockPrice(event: Event) {
     event.preventDefault();
-    // const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
-    const stockSymbol = this.stockInput.value;
+    // const stockUserInput = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    const stockSymbol = this.stockInput.value
     console.log(event);
     const { data } =
       await axios.get(
@@ -28,8 +39,14 @@ export class StockPrice {
   render() {
     return [
       <form onSubmit={this.onFetchStockPrice.bind(this)}>
-        <input id="stock-symbol" type='text' ref={el => this.stockInput = el }/>
-        <button type="submit">Fetch</button>
+        <input
+          id="stock-symbol"
+          type='text'
+          ref={el => this.stockInput = el }
+          value={this.stockUserInput}
+          onInput={this.onUserInput.bind(this)}
+        />
+        <button type="submit" disabled={!this.stockInputValid}>Fetch</button>
       </form>,
       <div>
         <p>Price: ${this.fetchedPrice}</p>
